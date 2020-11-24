88974cfeea2070d24dfa94de9e562ed1487dd98c
GoodOasis <de.thibault@gmail.com>
2020-11-25 00:12:26 +0100

    Test for django template langage

--- src/devise/views.py
+++ src/devise/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "devise/index.html")
+    return render(request, "devise/index.html", context={"test":5, "list_nombre": range(10)})
